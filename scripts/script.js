2a01c38ffa3b6e3de8f63e9db5391e9c1a420d3d
D4N-MC <danmezacadillo@gmail.com>
2023-09-25 20:34:25 -0500

    feat/mediaqueriesadded

--- scripts/script.js
+++ scripts/script.js
@@ -4,17 +4,8 @@ let PerfilNombre = document.querySelector(".user__top-name");
 let PerfilAbout = document.querySelector(".user__bottom");
 
 function OpenFormulario(){
-    formulario.innerHTML =
-       `<div class="Edit">
-            <div class="block">
-                <img class="block__close" src="images/iconos/Close Icon.svg" alt="Cerrar formulario">
-                <img class="block__close-hover" src="images/iconos/Close Icon Hover.svg" alt="Cerrar formulario">
-                <h4 class="block__text">Editar perfil</h4>
-                <input type="text" id="nombre" placeholder="Nombre">
-                <input type="text" id="about" placeholder="Acerca de mi">
-                <button type="button" class="Guardar">Guardar</button>
-            </div>
-        </div>`
+        formulario.classList.add("popup_opened");
+
         let closebutton = document.querySelector(".block__close");
         closebutton.addEventListener("click", CloseFormulario);
 
@@ -45,7 +36,7 @@ function OpenFormulario(){
 }
 
 function CloseFormulario(){
-    formulario.innerHTML = "";
+    formulario.classList.remove("popup_opened");
 }
 
 
