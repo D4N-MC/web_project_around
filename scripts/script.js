9b9a9db8778207d636f5b34cd856d1b9bc0ff597
D4N-MC <danmezacadillo@gmail.com>
2023-09-26 21:39:17 -0500

    pretier code and some corrections

--- scripts/script.js
+++ scripts/script.js
@@ -3,50 +3,49 @@ let formulario = document.querySelector(".formulario");
 let PerfilNombre = document.querySelector(".user__top-name");
 let PerfilAbout = document.querySelector(".user__bottom");
 
-function OpenFormulario(){
-        formulario.classList.add("popup_opened");
+function OpenFormulario() {
+  formulario.classList.add("popup_opened");
 
-        let closebutton = document.querySelector(".block__close");
-        closebutton.addEventListener("click", CloseFormulario);
+  let closebutton = document.querySelector(".block__close");
+  closebutton.addEventListener("click", CloseFormulario);
 
-        let nombre = document.getElementById("nombre");
-        let about = document.getElementById("about");
-        nombre.value = PerfilNombre.textContent;
-        about.value = PerfilAbout.textContent;
+  let nombre = document.getElementById("nombre");
+  let about = document.getElementById("about");
+  nombre.value = PerfilNombre.textContent;
+  about.value = PerfilAbout.textContent;
 
-        let submit = document.querySelector(".Guardar");
+  let submit = document.querySelector(".Guardar");
 
-        function CheckGuardar() { if(nombre.value === "" || about.value === ""){
-            submit.classList.remove("Guardar");
-            submit.classList.add("Guardar-disabled");
-            }else{
-                submit.classList.add("Guardar");
-                submit.classList.remove("Guardar-disabled");
-                submit.addEventListener("click", SubmitFormulario);
-            }
-        }
+  function CheckGuardar() {
+    if (nombre.value === "" || about.value === "") {
+      submit.classList.remove("Guardar");
+      submit.classList.add("Guardar-disabled");
+    } else {
+      submit.classList.add("Guardar");
+      submit.classList.remove("Guardar-disabled");
+      submit.addEventListener("click", SubmitFormulario);
+    }
+  }
 
-        CheckGuardar();
+  CheckGuardar();
 
-        nombre.addEventListener("input", CheckGuardar);
-        about.addEventListener("input", CheckGuardar);
+  nombre.addEventListener("input", CheckGuardar);
+  about.addEventListener("input", CheckGuardar);
 
-        submit.addEventListener("click", SubmitFormulario);
-
+  submit.addEventListener("click", SubmitFormulario);
 }
 
-function CloseFormulario(){
-    formulario.classList.remove("popup_opened");
+function CloseFormulario() {
+  formulario.classList.remove("popup_opened");
 }
 
+function SubmitFormulario() {
+  let nombreValue = document.getElementById("nombre").value;
+  let aboutValue = document.getElementById("about").value;
 
-function SubmitFormulario(){
-    let nombreValue = document.getElementById("nombre").value;
-    let aboutValue = document.getElementById("about").value;
-        
-    PerfilNombre.textContent = nombreValue;
-    PerfilAbout.textContent = aboutValue;
-    CloseFormulario();
+  PerfilNombre.textContent = nombreValue;
+  PerfilAbout.textContent = aboutValue;
+  CloseFormulario();
 }
 
 editbutton.addEventListener("click", OpenFormulario);
